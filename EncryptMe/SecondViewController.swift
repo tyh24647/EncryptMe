e970d260075d95a59b07ef1a7e4e5b5f25b22cdb
tyh24647 <tyhostager@yahoo.com>
2017-01-23 14:16:32 -0600

    Initial Commit

---
+++ EncryptMe/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EncryptMe
+//
+//  Created by Tyler hostager on 1/23/17.
+//  Copyright © 2017 Tyler Hostager. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
